06514496f09e268fa6420a7bee75ef39d13c11c3
Tyler Neylon <tylerneylon@gmail.com>
2018-11-24 16:00:17 -0800

    Add parameter names.
    And a visual separator between key sections.

--- dvidump.py
+++ dvidump.py
@@ -50,8 +50,9 @@ def make_opcode(name=None, comment='', params=[]):
             'params': params
     }
 
-def make_p(p_len, p_type):
+def make_p(p_name, p_len, p_type):
     return {
+            'name': p_name,
             'len': p_len,
             'type': p_type
     }
@@ -67,33 +68,33 @@ def init_opcodes():
     for i in range(128):
         opcodes.append(make_opcode(
                 name = 'set_char_%d' % i,
-                comment = chr(i).encode('ascii') if chr(i) in printable else ''
+                comment = chr(i) if chr(i) in printable else ''
         ))
 
     # 128-131 set[i]
     for i in [1, 2, 3, 4]:
         opcodes.append(make_opcode(
                 name = 'set%d' % i,
-                params = [make_p(i, 'U')]
+                params = [make_p('c', i, 'U')]
         ))
 
     # 132 set_rule
     opcodes.append(make_opcode(
             name = 'set_rule',
-            params = [make_p(4, 'S'), make_p(4, 'S')]
+            params = [make_p('a', 4, 'S'), make_p('b', 4, 'S')]
     ))
 
     # 133-136 put[i]
     for i in [1, 2, 3, 4]:
         opcodes.append(make_opcode(
                 name = 'put%d' % i,
-                params = [make_p(i, 'U')]
+                params = [make_p('c', i, 'U')]
         ))
 
     # 137 put_rule
     opcodes.append(make_opcode(
             name = 'put_rule',
-            params = [make_p(4, 'S'), make_p(4, 'S')]
+            params = [make_p('a', 4, 'S'), make_p('b', 4, 'S')]
     ))
 
     # 138 nop
@@ -102,8 +103,8 @@ def init_opcodes():
     ))
 
     # 139 bop
-    params = [make_p(4, 'U')] * 10
-    params.append(make_p(4, 'S'))
+    params = [make_p('c_%d' % i, 4, 'U') for i in range(10)]
+    params.append(make_p('p', 4, 'S'))
     opcodes.append(make_opcode(
             name = 'bop',
             params = params
@@ -128,7 +129,7 @@ def init_opcodes():
     for i in [1, 2, 3, 4]:
         opcodes.append(make_opcode(
                 name = 'right%d' % i,
-                params = [make_p(i, 'S')]
+                params = [make_p('b', i, 'S')]
         ))
 
     # 147 w0
@@ -136,14 +137,13 @@ def init_opcodes():
             name = 'w0'
     ))
 
-    # XXX
     assert len(opcodes) == 148
 
     # 148-151 w[i]
     for i in [1, 2, 3, 4]:
         opcodes.append(make_opcode(
                 name = 'w%d' % i,
-                params = [make_p(i, 'S')]
+                params = [make_p('b', i, 'S')]
         ))
 
     # 152 x0
@@ -155,14 +155,14 @@ def init_opcodes():
     for i in [1, 2, 3, 4]:
         opcodes.append(make_opcode(
                 name = 'x%d' % i,
-                params = [make_p(i, 'S')]
+                params = [make_p('b', i, 'S')]
         ))
 
     # 157-160 down[i]
     for i in [1, 2, 3, 4]:
         opcodes.append(make_opcode(
                 name = 'down%d' % i,
-                params = [make_p(i, 'S')]
+                params = [make_p('a', i, 'S')]
         ))
 
     # 161 y0
@@ -174,7 +174,7 @@ def init_opcodes():
     for i in [1, 2, 3, 4]:
         opcodes.append(make_opcode(
                 name = 'y%d' % i,
-                params = [make_p(i, 'S')]
+                params = [make_p('a', i, 'S')]
         ))
 
     # 166 z0
@@ -186,7 +186,7 @@ def init_opcodes():
     for i in [1, 2, 3, 4]:
         opcodes.append(make_opcode(
                 name = 'z%d' % i,
-                params = [make_p(i, 'S')]
+                params = [make_p('a', i, 'S')]
         ))
 
     # 171-234 fnt_num_[i]
@@ -199,43 +199,42 @@ def init_opcodes():
     for i in [1, 2, 3, 4]:
         opcodes.append(make_opcode(
                 name = 'fnt%d' % i,
-                params = [make_p(i, 'U')]
+                params = [make_p('k', i, 'U')]
         ))
 
     # 239-242 xxx[i]
     for i in [1, 2, 3, 4]:
         opcodes.append(make_opcode(
                 name = 'xxx%d' % i,
-                params = [make_p(i, 'U'), make_p([0], 'str')]
+                params = [make_p('k', i, 'U'), make_p('x', [0], 'str')]
         ))
 
     # 243-246 fnt_def[i]
     for i in [1, 2, 3, 4]:
         params = [
-                make_p(i, 'U'),        # k
-                make_p(4, 'U'),        # c
-                make_p(4, 'U'),        # s
-                make_p(4, 'U'),        # d
-                make_p(1, 'U'),        # a
-                make_p(1, 'U'),        # l
-                make_p([4, 5], 'str')  # n
+                make_p('k', i, 'U'),
+                make_p('c', 4, 'U'),
+                make_p('s', 4, 'U'),
+                make_p('d', 4, 'U'),
+                make_p('a', 1, 'U'),
+                make_p('l', 1, 'U'),
+                make_p('n', [4, 5], 'str')
         ]
         opcodes.append(make_opcode(
                 name = 'fnt_def%d' % i,
                 params = params
         ))
 
-    # XXX
     assert len(opcodes) == 247
 
     # 247 pre
     params = [
-            make_p(1, 'U'),     # i
-            make_p(4, 'U'),     # num
-            make_p(4, 'U'),     # den
-            make_p(4, 'U'),     # mag
-            make_p(1, 'U'),     # k
-            make_p([4], 'str')  # x
+            make_p('i',   1, 'U'),
+            make_p('num', 4, 'U'),
+            make_p('den', 4, 'U'),
+            make_p('mag', 4, 'U'),
+            make_p('k',   1, 'U'),
+            make_p('x',   [4], 'str')
     ]
     opcodes.append(make_opcode(
             name = 'pre',
@@ -244,14 +243,14 @@ def init_opcodes():
 
     # 248 post
     params = [
-            make_p(4, 'U'),  # p
-            make_p(4, 'U'),  # num
-            make_p(4, 'U'),  # den
-            make_p(4, 'U'),  # mag
-            make_p(4, 'U'),  # l
-            make_p(4, 'U'),  # u
-            make_p(2, 'U'),  # s
-            make_p(2, 'U')   # t
+            make_p('p',   4, 'U'),
+            make_p('num', 4, 'U'),
+            make_p('den', 4, 'U'),
+            make_p('mag', 4, 'U'),
+            make_p('l',   4, 'U'),
+            make_p('u',   4, 'U'),
+            make_p('s',   2, 'U'),
+            make_p('t',   2, 'U')
     ]
     opcodes.append(make_opcode(
             name = 'post',
@@ -261,7 +260,7 @@ def init_opcodes():
     # 249 post_post
     opcodes.append(make_opcode(
             name = 'post_post',
-            params = [make_p(4, 'U'), make_p(1, 'U')]
+            params = [make_p('q', 4, 'U'), make_p('i', 1, 'U')]
     ))
 
     assert len(opcodes) == 250
@@ -298,11 +297,12 @@ def read_val(f, val_len, val_type):
 
     assert False
 
-def get_val_str(val, val_type):
-    if val_type is 'str':
-        return val.decode('utf-8')
+def get_val_str(param, val):
+    if param['type'] is 'str':
+        val_s = val.decode('utf-8')
     else:
-        return '%d' % val
+        val_s = '%d' % val
+    return '%3s   %s' % (param['name'], val_s)
 
 def print_next_opcode(f):
 
@@ -322,7 +322,11 @@ def print_next_opcode(f):
     for param in params:
         val_len, val_type = get_val_len(param, vals)
         vals.append(read_val(f, val_len, val_type))
-        print(' ' * 21 + get_val_str(vals[-1], val_type))
+        print(' ' * 21 + get_val_str(param, vals[-1]))
+
+    # After key opcodes, we print out a clear visual separator.
+    if name in ['eop', 'pre']:
+        print('\n' + '_' * 70 + '\n')
 
     if name == 'post_post':
         opcodes[223] = make_opcode(name='final_pad')
